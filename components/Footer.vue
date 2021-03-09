c16d9bfb3b14009ff666652c2f9e781123e9b61e
liangboliang <liangboliang@yy.com>
2021-03-09 14:35:44 +0800

    fix: 修改样式

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,11 +4,11 @@
       <div class="footer-about">
         <h1>关于我们</h1>
         <div>
-          深圳市北富联机电有限公司（www.northfull.com）是一家专业进口高端机电零部件销售、服务为一体的专业公司，为客户提供高品质五金紧固件工业服务商!
+          深圳市北富联机电有限公司（www.northfull.com）是一家工业高精密紧固件销售、服务为一体的专业公司，为客户提供高品质五金紧固件工业服务商！
         </div>
         <div class="mt30">
-          北富联机电位于深圳市宝安区，提供McMASER-CARR中国代理(高端螺丝和螺栓，螺纹杆和螺柱，吊环螺栓,U型螺栓,坚果类,垫圈,垫片，螺旋和螺纹刀片，垫片和支座，销，锚点,钉，射钉枪，柳钉，柳钉工具，斯台普斯，订书机，关键股票等)、Helicoil螺套（金属螺纹嵌件，塑料螺纹嵌件，木螺纹嵌件，复合材料的螺纹嵌件，紧固件分类，管状腿的调平安装插件等）、BERG连接器、RECOIL螺纹护套及配套机电工具等。
-          本公司以“用心服务，不断提升自我”为宗旨，坚持以用户的要求为中心，提供高效服务，为客户解决问题，北富联机电致力于为客户提供高效服务的五金机电服务商
+          北富联机电提供ISO国际、美国米制ANSI（M）、ASME（M）、ASTM（M）、JIS日本、DIN德国、EN欧盟、GB中国、BS英国（M）、AS（M）澳大利亚的系列标准件。主要代理McMASER-CARR、Helicoil螺套、BERG定位销、RECOIL螺纹护套及配套机电工具等。
+          本公司以“用心服务，客户为核心”为宗旨，坚持以用户的要求为中心，提供高效服务，为客户解决问题。北富联机电是您值得信赖的合作伙伴！
         </div>
       </div>
       <div>
@@ -18,7 +18,9 @@
             footerDetail.phone
           }}
         </div>
+        <div class="mtb20"><a-icon type="qq" class="icon" />：318203871</div>
         <div class="mtb20">企业邮箱：{{ footerDetail.email }}</div>
+
         <div><img :src="footerDetail.weixinImg" alt="" /></div>
       </div>
     </div>
