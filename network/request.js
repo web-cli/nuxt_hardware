d62bda1e7ffabf2e2610098eb478dc9762df92ce
liangboliang <liangboliang@joyy.sg>
2021-06-15 17:25:30 +0800

    feat: 新增环境IP判断

--- network/request.js
+++ network/request.js
@@ -1,29 +1,27 @@
 import axios from 'axios'
-
+import { IS_DEV } from '@/assets/js/env'
 function request(config) {
-
   const instance = axios.create({
     // baseURL: 'http://101.201.199.139/web/api',
-    baseURL: 'http://129.204.140.71:3333/web/api' || '/web/api',
-    timeout: 5000
+    baseURL: IS_DEV ? 'http://localhost:3333/web/api' : '/web/api',
+    timeout: 5000,
   })
 
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
+  instance.interceptors.request.use(
+    (config) => {
+      return config
+    },
+    (err) => {}
+  )
 
-  })
-
-  instance.interceptors.response.use(res => {
-    return res
-  }, err => {
-
-  })
+  instance.interceptors.response.use(
+    (res) => {
+      return res
+    },
+    (err) => {}
+  )
 
   return instance(config)
-
 }
 
-export {
-  request
-}
+export { request }
