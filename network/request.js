4c8cab8ef75ca335257a6549d4d62a0d7ea34e41
liangboliang <liangboliang@joyy.sg>
2021-06-15 19:17:18 +0800

    feat: 发布

--- network/request.js
+++ network/request.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { IS_DEV } from '@/assets/js/env'
-console.log(IS_DEV, 69)
+const url = 'http://localhost:3333/web/api'
 function request(config) {
   const instance = axios.create({
     // baseURL: 'http://101.201.199.139/web/api',
-    baseURL: IS_DEV ? 'http://localhost:3333/web/api' : '/web/api',
+    baseURL: '/web/api',
     timeout: 5000,
   })
 
