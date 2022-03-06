4ccbd8b50097a774f12d704fb395f3a11f5c484f
boliangleung <786981510@qq.com>
2022-03-07 00:17:51 +0800

    feat: 修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'my-nuxt',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
